d9712f7f3241ff3e331b23c39f1591084ff40090
Rami ahmed <35673698+ramisadek@users.noreply.github.com>
2019-11-18 16:36:16 +0200

    change from 2000 to steps

--- lib/pages/others/map.dart
+++ lib/pages/others/map.dart
@@ -677,7 +677,7 @@ class _MapPageState extends State<MapPage> {
                                     "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
                                 formdata.add("distance",
                                     (distance * 1000).toInt());
-                                formdata.add("steps", 20000);
+                                formdata.add("steps", steps);
                                 formdata.add("calories",
                                     ((steps * 0.0512).ceil()).toInt());
                                 print(
