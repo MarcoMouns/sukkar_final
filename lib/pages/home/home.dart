5422061906014793baf951401f868a73c732ba33
Omar Sherif <omar.sherifs@live.com>
2020-06-08 11:32:35 +0200

    add long text handling to home

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1097,18 +1097,25 @@ class _HomePageState extends State<HomePage> {
                                                                           right:
                                                                               10),
                                                                       child:
-                                                                          Text(
-                                                                        banners[index]
-                                                                            .name,
-                                                                        style: TextStyle(
-                                                                            color: Color.fromRGBO(
-                                                                                41,
-                                                                                172,
-                                                                                216,
-                                                                                1),
-                                                                            fontSize:
-                                                                                15),
-                                                                      ),
+                                                                    Container(
+                                                            width: MediaQuery.of(context).size.width *
+                                                                            0.355,
+                                                                        child:
+                                                                            Text(
+                                                                          banners[index]
+                                                                              .name,
+                                                                          softWrap:
+                                                                              true,
+                                                                          overflow:
+                                                                              TextOverflow.ellipsis,
+                                                                          style: TextStyle(
+                                                                              color: Color.fromRGBO(41, 172, 216,
+                                                                      1),
+                                                                  fontSize:
+                                                                  15),
+
+                                                            ),
+                                                          ),
                                                                     ),
                                                                     Padding(
                                                                       padding: EdgeInsets
